32dda04eef14496e206633322053dd697207b585
Srijan Bandreddi <bsrijan@gmail.com>
2025-04-05 19:05:55 +0200

    created smart order module

---
+++ choco/smart_order/doctype/incoming_customer_call/incoming_customer_call.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Srijan Bandreddi and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Incoming Customer Call", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ choco/smart_order/doctype/incoming_customer_call/test_incoming_customer_call.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Srijan Bandreddi and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestIncomingCustomerCall(FrappeTestCase):
+	pass
