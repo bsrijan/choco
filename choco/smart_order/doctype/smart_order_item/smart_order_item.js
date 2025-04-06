5cb8155201211b9ab41cd8c61b62290f530267d5
Srijan Bandreddi <bsrijan@gmail.com>
2025-04-06 11:14:33 +0200

    feat: smart order item

---
+++ choco/smart_order/doctype/smart_order_item/smart_order_item.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Srijan Bandreddi and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Smart Order Item", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ choco/smart_order/doctype/smart_order_item/smart_order_item.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Srijan Bandreddi and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class SmartOrderItem(Document):
+	pass
